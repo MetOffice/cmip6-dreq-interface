9281b01dc321b8b3267c8eb5741b8626284f6da4
Tim Bradshaw <tim.bradshaw@metoffice.gov.uk>
2016-03-15 16:04:03 +0000

    A rudimentary setup script

---
+++ package/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name="dqi",
+      version="0.0",
+      description="Interfaces to the CMIP6 data request",
+      author="Tim Bradshaw",
+      author_email="tim.bradshaw@metoffice.gov.uk",
+      packages=find_packages(),
+      scripts=("bin/jwalk", "bin/twalk",
+               "bin/m2v", "bin/v2m",
+               "bin/dqcsv"),
+      zip_safe=False)
