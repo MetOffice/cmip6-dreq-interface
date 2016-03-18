c896a69303a5ded504a4ee673e33f8b5b5a8d68a
Tim Bradshaw <tim.bradshaw@metoffice.gov.uk>
2016-03-18 12:52:05 +0000

    All command-line programs let you specify where the dreq is
    I factored out the directory-merging function into dqi.low from dqcsv,
    and chang ed jwalk and twalk to use it.

--- package/dqi/low.py
+++ package/dqi/low.py
@@ -2,6 +2,16 @@
 
 __all__ = ['Badness']
 
+from os.path import split, join
+
 class Badness(Exception):
     pass
 
+def reroot_paths(root, paths):
+    # given a new root /d and a bunch of paths .../a, .../b return
+    # /d/a, /d/b and so on
+    if root is None:
+        return paths
+    else:
+        return tuple(join(root, split(path)[1])
+                     for path in paths)
