5f22915bd9efc136a95d50ab4c70c4396b1125d6
Tim Bradshaw <tim.bradshaw@metoffice.gov.uk>
2016-06-07 16:05:14 +0100

    djq: initial setup.py

---
+++ djq/setup.py
@@ -0,0 +1,15 @@
+# DREQ JSON Query: rudimentary setuptools config
+#
+
+from setuptools import setup, find_packages
+
+setup(name="djq",
+      version="0.0",
+      description="JSON queries for the CMIP6 data request",
+      author="Tim Bradshaw",
+      author_email="tim.bradshaw@metoffice.gov.uk",
+      packages=find_packages("lib"),
+      package_dir={"":"lib"},
+      scripts=(),
+      test_suite='nose.collector',
+      zip_safe=False)
