1d95fe872e0d3a1d7cca82c969962bf4b1964345
Tim Bradshaw <tim.bradshaw@metoffice.gov.uk>
2016-07-07 18:34:11 +0100

    djq: package tests for djq.variables

---
+++ djq/lib/djq/variables/package_tests.py
@@ -0,0 +1,20 @@
+# Package tests for djq
+#
+
+from types import ModuleType, FunctionType
+import djq.variables as variables
+from djq.low import validate_package_interface, report_package_interface
+
+categories = {'instances': {FunctionType: ('compute_variables',
+                                           'jsonify_variables',
+                                           'cv_implementation')},
+              'types': {Exception: ('NoExperiment', 'NoMIP',
+                                    'WrongExperiment', 'BadCVImplementation')}}
+
+def test_interface():
+    if not validate_package_interface(variables, categories):
+        report_package_interface(variables, categories)
+        raise Exception("djq.variable package interface trouble")
+
+if __name__ == '__main__':
+    report_package_interface(variables, categories)
