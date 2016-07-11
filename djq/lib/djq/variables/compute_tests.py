26a58534c176b066f9bc596a41d2be2b1a15a3a7
Tim Bradshaw <tfb@cley.com>
2016-07-11 13:11:16 +0100

    djq: tests for implementation switching

---
+++ djq/lib/djq/variables/compute_tests.py
@@ -0,0 +1,25 @@
+# Minimal tests of compute
+#
+
+from sys import modules
+from nose.tools import raises
+from compute import cv_implementation, BadCVImplementation
+import impl_dreq_example
+
+# Check implementation switching
+
+me = modules[__name__]
+
+@raises(BadCVImplementation)
+def test_switch_to_me():
+    cv_implementation(me)
+
+def test_switch_to_good():
+    cv_implementation(impl_dreq_example)
+    assert cv_implementation() is impl_dreq_example
+
+def test_switch_to_fn():
+    def impl(dq, mip, exids):
+        return set()
+    cv_implementation(impl)
+    assert cv_implementation() is impl
