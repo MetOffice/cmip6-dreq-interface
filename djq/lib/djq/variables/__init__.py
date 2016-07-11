62df365383b9031b7bfb9bb88f326063c10e08e7
Tim Bradshaw <tfb@cley.com>
2016-07-11 13:30:30 +0100

    djq: it should just import a default implementation
    It's up to users to import any others.  The default implementation is
    just a symlink so it's easy to change.

--- djq/lib/djq/variables/__init__.py
+++ djq/lib/djq/variables/__init__.py
@@ -4,16 +4,11 @@
 from . import compute
 from .compute import *
 
-# Implementations
+# Back ends: just import the default back end (which is a symlink)
+# and set it up.
 #
-# The example back end
-from . import impl_dreq_example
-
-# A back end which tries to invert the variable->mip map
-from . import impl_invert_varmip
-
-# Set the default implementation
-cv_implementation(impl_dreq_example)
+from . import impl_default
+cv_implementation(impl_default)
 
 # jsonifying variable
 from . import jsonify
