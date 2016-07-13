0c1f2c86b7e246ad90d404e985cd09312db5d8ce
Tim Bradshaw <tfb@cley.com>
2016-07-13 12:47:14 +0100

    djq: more jsonify switch support
    Allow selection from command line
    
    Add checker support

--- djq/lib/djq/variables/jsonify.py
+++ djq/lib/djq/variables/jsonify.py
@@ -6,9 +6,15 @@ __all__ = ('jsonify_variables', 'jsonify_implementation',
 
 from threading import local
 from djq.low import whisper, ExternalException, Scram, Disaster
+from djq.low import make_checktree, checker
 
 class BadJSONifyImplementation(ExternalException):
     pass
+
+# checkers (which are not, yet, per backend) are run on the way out,
+# and are called with the dq, cmvids, and the JSON structure resulting
+#
+checks = make_checktree()
 
 state = local()
 state.implementation = None
@@ -53,6 +59,8 @@ def jsonify_variables(dq, cmvids):
     (jsonify_cmvids, impl) = effective_jsonify_implementation()
     results = sorted(jsonify_cmvids(dq, cmvids),
                      key=lambda j: j['label'])
+    if checks(args=(dq, cmvids, results)) is False:
+        raise Disaster("failed JSONify checks")
     for r in results:
         whisper("     {}", r['label'])
     return results
