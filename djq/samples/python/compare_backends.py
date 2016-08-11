9507f8c9aa09136fe0eec3ace4d89cda751d0b55
Tim Bradshaw <tim.bradshaw@metoffice.gov.uk>
2016-08-11 16:03:37 +0100

    djq: add default implemenation to comparison sample

--- djq/samples/python/compare_backends.py
+++ djq/samples/python/compare_backends.py
@@ -7,13 +7,14 @@ in order to compare their results.
 """
 
 from djq import process_request, ensure_dq
+from djq.variables import cv_implementation
 import djq.variables.cv_dreq_example as cde
 import djq.variables.cv_invert_varmip as civ
 
 request = [{'mip': "AerChemMIP",
             'experiment': "HISTghg"}]
 
-for impl in (cde, civ):
+for impl in (cv_implementation(), cde, civ):
     print ("with {}: {} variables"
            .format(impl.__name__,
                    len(process_request(request,
